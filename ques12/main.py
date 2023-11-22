5d137d436602ae9953d3388d8d6b357f75972b53
sanchxt <92094343+sanchxt@users.noreply.github.com>
2023-11-23 03:17:41 +0530

    Add files via upload

---
+++ ques12/main.py
@@ -0,0 +1,6 @@
+from module import large
+
+val1 = float(input('First value: '))
+val2 = float(input('Second value: '))
+
+print(f'Larger value of the 2 entered numbers is: {large(val1, val2)}')
---
+++ ques12/module.py
@@ -0,0 +1,2 @@
+def large(a, b):
+    return max(a, b)
